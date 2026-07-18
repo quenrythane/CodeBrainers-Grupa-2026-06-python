66f94efc01d64263e05d88707a0cb27c9030fe4e
quenrythane <artur.babinski.g@gmail.com>
2026-07-18 10:15:52 +0200

    feat: add functional programming practice scripts and configure .gitignore for Python caches

---
+++ 09_programowanie_funkcyjne/moj_program.py
@@ -0,0 +1,16 @@
+# import funkcja_robienia_herbaty
+# funkcja_robienia_herbaty.zrob_herbate("zielona", True)
+
+
+# import funkcja_robienia_herbaty as herbaciarz
+# herbaciarz.zrob_herbate("zielona", True)
+
+
+from funkcja_robienia_herbaty import zrob_herbate
+# from funkcja_robienia_herbaty import zrob_herbate, zrob_kawe
+# from funkcja_robienia_herbaty import *
+zrob_herbate("zielona", True)
+
+
+# from funkcja_robienia_herbaty import zrob_herbate as czas_na_herbatke
+# czas_na_herbatke("zielona", True)
