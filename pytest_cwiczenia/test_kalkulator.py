a8d46bc72705e2d0f29ddbc2436e8e68ffcf90c4
quenrythane <artur.babinski.g@gmail.com>
2026-07-21 18:26:26 +0200

    feat: implement calculator arithmetic functions with pytest test suite and update gitignore

--- pytest_cwiczenia/test_kalkulator.py
+++ pytest_cwiczenia/test_kalkulator.py
@@ -21,10 +21,13 @@
 # python -m pytest -v test_kalkulator.py::test_add_positive test_kalkulator2.py::test_add_negative
 
 
-from kalkulator import add
+from kalkulator import add, odejmowanie
 
 # konwencja nazwy funkcji testowej test_<nazwa_testowanej_funkcji>
 # print("\nxd xd xd")
+
+
+
 def test_add_positive():
     assert add(2, 2) == 4
     assert add(0, 2) == 2
@@ -38,3 +41,10 @@ def test_add_negative():
     assert add(-2, 0) == -2
     assert add(-2, 2) == 0
     assert add(-2, 20) == 18
+
+def test_odejmowanie():
+    assert odejmowanie(5, 2) == 3
+    assert odejmowanie(0, 2) == -2
+    assert odejmowanie(0, 0) == 0
+    assert odejmowanie(10, 0.5) == 9.5
+
