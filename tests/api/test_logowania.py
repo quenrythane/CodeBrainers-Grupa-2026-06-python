5ae00e22a3be875cba41f458dd141ba93dc5d6ca
quenrythane <artur.babinski.g@gmail.com>
2026-07-30 18:43:39 +0200

    testy autentykacji

---
+++ tests/api/test_logowania.py
@@ -0,0 +1,43 @@
+import requests
+import pytest
+
+
+
+
+@pytest.mark.xd
+def test_auth_token():
+    global token
+    """Fixture pobierający token autoryzacyjny (accessToken) dla domyślnego użytkownika."""
+    login_url = 'https://dummyjson.com/auth/login'
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    payload = {
+        "username": "michaelw",
+        "password": "michaelwpass"
+    }
+
+    response = requests.post(login_url, headers=headers, json=payload)
+    token = response.json()['accessToken']
+    print(token)
+
+
+@pytest.mark.xd
+def test_get_user():
+    global token
+    url = "https://dummyjson.com/auth/me"
+
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+
+    response = requests.get(url, headers=headers)
+    response_body = response.json()
+    print(response.status_code)
+    print(response_body['firstName'])
+    print(response_body['lastName'])
+    print(response_body['email'])
+
+
