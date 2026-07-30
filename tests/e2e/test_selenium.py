8d6d19cf6ff40c2b21e831180d5bb7990ee09156
quenrythane <artur.babinski.g@gmail.com>
2026-07-31 00:07:12 +0200

    clean markers and refactor test

--- tests/e2e/test_selenium.py
+++ tests/e2e/test_selenium.py
@@ -1,19 +1,34 @@
+from dataclasses import dataclass
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 # id - preferowany (unikalny)
 # class LUB name - preferowane, jezeli sa unikalne
 # xpath - preferowany, jezeli id nie ma lub nie jest unikalny
 
+@dataclass
+class User:
+    id: str = "20"
+    name: str = "Asia"
+    salary: str = "1000"
+    age: str = "18"
+    position: str = "Junior QA"
+    on_leave: str = "✅"
+
+
 @pytest.fixture
 def driver():
     URL = "http://127.0.0.1:8000/"
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
     driver.get(URL)
     yield driver
     driver.quit()
@@ -21,17 +36,7 @@ def driver():
 
 @pytest.fixture
 def user():
-    class User:
-        def __init__(self):
-            self.id = "1"
-            self.name = "Asia"
-            self.salary = "1000"
-            self.age = "18"
-            self.position = "Junior QA"
-            self.on_leave = "✅"
-
-    user = User()
-    return user
+    return User()
 
 
 @pytest.mark.skip
@@ -47,7 +52,7 @@ def test_selenium(driver):
     # input("Wcisnij enter zeby zamknac")
 
 
-@pytest.mark.seler
+@pytest.mark.selenium
 def test_add_user(driver, user):
     # Arrange
 
@@ -58,14 +63,35 @@ def test_add_user(driver, user):
     Select(driver.find_element(By.ID, "position")).select_by_value(user.position)
     driver.find_element(By.ID, "on_leave").click()
     driver.find_element(By.ID, "submitBtn").click()
-    sleep(1)
 
-    #Assert
-    elements = driver.find_elements(By.TAG_NAME, "td")  # return a list [Asia, 1000, 18, Junior QA]
-    assert user.id in elements[0].text
-    assert user.name in elements[1].text
-    assert user.salary in elements[2].text
-    assert user.age in elements[3].text
-    assert user.position in elements[4].text
-    assert user.on_leave in elements[5].text
+    # Assert
+    WebDriverWait(
+        driver,
+        timeout=10,
+        poll_frequency=0.5
+    ).until(EC.visibility_of_element_located((By.TAG_NAME, "table")))
+
+
+    table_row = [row.find_elements(By.TAG_NAME, "td") for row in driver.find_elements(By.TAG_NAME, "tr") if row.text.split(" ")[0] == user.id][0]
+    assert user.id in table_row[0].text
+    assert user.name in table_row[1].text
+    assert user.salary in table_row[2].text
+    assert user.age in table_row[3].text
+    assert user.position in table_row[4].text
+    assert user.on_leave in table_row[5].text
+
+    # table_rows = driver.find_elements(By.TAG_NAME, "tr")  # pobieramy wiersze tabeli
+    # for row in table_rows:
+    #     table_cells = driver.find_elements(By.TAG_NAME, "td")  # pobieramy komórki Z TEGO WIERSZA
+    #     if table_cells[0].text == user.id:
+    #         assert user.id in table_cells[0].text
+    #         assert user.name in table_cells[1].text
+    #         assert user.salary in table_cells[2].text
+    #         assert user.age in table_cells[3].text
+    #         assert user.position in table_cells[4].text
+    #         assert user.on_leave in table_cells[5].text
+            # break
+
+
+
 
