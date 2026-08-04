17e1766808d4b9f32a8f3beaf396efb19adc752d
quenrythane <artur.babinski.g@gmail.com>
2026-08-04 19:14:04 +0200

    przełączenie do page object model

---
+++ tests/e2e/pageObjects/base_page.py
@@ -0,0 +1,47 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# POM - Page Object Model
+
+# id - preferowany (unikalny)
+# class LUB name - preferowane, jezeli sa unikalne
+# xpath - preferowany, jezeli id nie ma lub nie jest unikalny
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    URL = "http://127.0.0.1:8000/"
+    NAME_INPUT_LOCATOR = (By.ID, "name")
+    SALARY_INPUT_LOCATOR = (By.ID, "salary")
+    AGE_INPUT_LOCATOR = (By.ID, "age")
+    POSITION_SELECT_LOCATOR = (By.ID, "position")
+    ON_LEAVE_CHECKBOX_LOCATOR = (By.ID, "on_leave")
+    SUBMIT_BUTTON_LOCATOR = (By.ID, "submitBtn")
+    TABLE_ROW_LOCATOR = (By.TAG_NAME, "tr")
+    TABLE_CELL_LOCATOR = (By.TAG_NAME, "td")
+
+    def fill_employee_form(self, user_data):
+        self.driver.find_element(*self.NAME_INPUT_LOCATOR).send_keys(user_data.name)
+        self.driver.find_element(*self.SALARY_INPUT_LOCATOR).send_keys(user_data.salary)
+        self.driver.find_element(*self.AGE_INPUT_LOCATOR).send_keys(user_data.age)
+        Select(self.driver.find_element(*self.POSITION_SELECT_LOCATOR)).select_by_value(user_data.position)
+        if user_data.on_leave == "✅":
+            self.driver.find_element(*self.ON_LEAVE_CHECKBOX_LOCATOR).click()
+        self.driver.find_element(*self.SUBMIT_BUTTON_LOCATOR).click()
+
+    def get_last_table_row(self):
+        table_row = WebDriverWait(
+            self.driver,
+            timeout=2,
+            poll_frequency=0.5
+        ).until(EC.visibility_of_element_located(self.TABLE_ROW_LOCATOR))
+        return table_row[-1]
+
+    def get_table_row_data(self, table_row):
+        return table_row.find_elements(*self.TABLE_CELL_LOCATOR)
+
--- tests/e2e/test_selenium.py
+++ tests/e2e/test_selenium.py
@@ -1,15 +1,6 @@
 import pytest
+from pageObjects.base_page import BasePage
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
-# id - preferowany (unikalny)
-# class LUB name - preferowane, jezeli sa unikalne
-# xpath - preferowany, jezeli id nie ma lub nie jest unikalny
 
 @pytest.mark.skip
 def test_selenium(driver):
@@ -20,21 +11,11 @@ def test_selenium(driver):
 
 @pytest.mark.selenium
 def test_add_user(driver, user):
-    driver.find_element(By.ID, "name").send_keys(user.name)
-    driver.find_element(By.ID, "salary").send_keys(user.salary)
-    driver.find_element(By.ID, "age").send_keys(user.age)
-    Select(driver.find_element(By.ID, "position")).select_by_value(user.position)
-    driver.find_element(By.ID, "on_leave").click()
-    driver.find_element(By.ID, "submitBtn").click()
+    base_page = BasePage(driver)
 
-    tablerow = WebDriverWait(
-        driver,
-        timeout=2,
-        poll_frequency=0.5
-    ).until(EC.visibility_of_element_located((By.TAG_NAME, "tr")))
-
-    last_row = driver.find_elements(By.TAG_NAME, "tr")[-1]
-    row_data = last_row.find_elements(By.TAG_NAME, "td")
+    base_page.fill_employee_form(user)
+    last_row = base_page.get_last_table_row()
+    row_data = base_page.get_table_row_data(last_row)
 
     assert row_data[1].text == user.name
     assert row_data[2].text == user.salary
